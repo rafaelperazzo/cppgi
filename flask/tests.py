25549c7cc64882ce04a7906d46fd58621d6cde05
Rafael Perazzo <rafael.mota@ufca.edu.br>
2022-07-28 14:08:58 -0300

    Iniciados testes automatizados. os primeiros

--- flask/tests.py
+++ flask/tests.py
@@ -7,19 +7,20 @@ usuario = config['DEFAULT']['usuario']
 senha = config['DEFAULT']['senha']
 usuario_senha = str.encode("%s:%s" %(usuario,senha))
 
+def get_res(res):
+    valid_credentials = base64.b64encode(usuario_senha).decode("utf-8")
+    rv = aplicacao.get(res,follow_redirects=True,headers={"Authorization": "Basic " + valid_credentials})
+    assert rv.status_code==200
+
 def test_main():
     rv = aplicacao.get('/',follow_redirects=True)
     assert rv.status_code==200
 
 def test_admin():
-    valid_credentials = base64.b64encode(usuario_senha).decode("utf-8")
-    rv = aplicacao.get('/admin',follow_redirects=True,headers={"Authorization": "Basic " + valid_credentials})
-    assert rv.status_code==200
+    get_res('/admin')
 
 def test_admin_edital():
-    valid_credentials = base64.b64encode(usuario_senha).decode("utf-8")
-    rv = aplicacao.get('/admin/9',follow_redirects=True,headers={"Authorization": "Basic " + valid_credentials})
-    assert rv.status_code==200
+    get_res('/admin/9')
 
 def test_edital_projeto():
     valid_credentials = base64.b64encode(usuario_senha).decode("utf-8")
@@ -28,6 +29,5 @@ def test_edital_projeto():
     """
     editais,total = executarSelect(consulta)
     for edital in editais:
-        rv = aplicacao.get('/editalProjeto/' + str(edital[0]),follow_redirects=True,headers={"Authorization": "Basic " + valid_credentials})
-        assert rv.status_code==200
-    
+        get_res('/editalProjeto/' + str(edital[0]))
+
